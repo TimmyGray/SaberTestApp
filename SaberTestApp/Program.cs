fc7e8715166e229b8578201281163d1b1fec5401
TimmyGray <zuuubada@inbox.ru>
2022-07-29 22:45:30 +0300

    set number of nodes and data of nodes

--- SaberTestApp/Program.cs
+++ SaberTestApp/Program.cs
@@ -2,9 +2,21 @@
 
 ListRandom newlist = new ListRandom();
 
-newlist.Add("Test Data 1");
-newlist.Add("Test Data 2");
-newlist.Add("Test Data 3");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Please, input the count of the listnodes:");
+Console.ForegroundColor = ConsoleColor.White;
+int i = Convert.ToInt32(Console.ReadLine());
+while (i>0)
+{
+    Console.Write("Please, input the data of the node:");
+    string nodedata = Console.ReadLine();
+    newlist.Add(nodedata);
+    i--;
+}
+
+//newlist.Add("Test Data 1");
+//newlist.Add("Test Data 2");
+//newlist.Add("Test Data 3");
 
 
 using (Stream filestream = new FileStream("./lists.dat", FileMode.OpenOrCreate, FileAccess.Write))
